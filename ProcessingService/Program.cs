6dbc969a65f6790f43dbf8c0fbe5480abda9767d
Terhon <terhon95@gmail.com>
2025-06-21 16:12:39 +0200

    change to .net ver 8.0

--- ProcessingService/Program.cs
+++ ProcessingService/Program.cs
@@ -2,16 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 app.MapControllers();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
