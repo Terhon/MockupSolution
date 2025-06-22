62941867d187bb89ff19e7df25d5a3cee4f0f813
Terhon <terhon95@gmail.com>
2025-06-22 21:12:04 +0200

    status code fix

--- ProcessingService/Controllers/TaskController.cs
+++ ProcessingService/Controllers/TaskController.cs
@@ -30,7 +30,7 @@ namespace ProcessingService.Controllers
         public IActionResult GetResult(string requestId)
         {
             if (!ResultStore.Data.TryGetValue(requestId, out var result))
-                return StatusCode(202, new { status = "Processing" });
+                return Accepted(new { status = "Processing" });
 
             return Ok(new { result });
         }
